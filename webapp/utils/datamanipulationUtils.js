5f1a6e6cd6d1f14f0fc82973b4d52bcdce848b0a
Milton Chandradas <milton.chandradas@sap.com>
2023-06-07 10:06:50 -0400

    changes...

---
+++ webapp/utils/datamanipulationUtils.js
@@ -0,0 +1,26 @@
+sap.ui.define([], function () {
+  "use strict";
+
+  return {
+    populateTableModel: function (lineModel, vizFrameModel) {
+      let rows = [];
+      let lines = lineModel.getData();
+
+      vizFrameModel.getData().Payscale.forEach((payscale) => {
+        let entry = {
+          position: payscale.Position,
+          compensation: payscale.Value,
+          minRange: lines[0].value,
+          current: lines[1].value,
+          recentHires: lines[2].value,
+          referencePoint: lines[3].value,
+          maxRange: lines[4].value,
+        };
+
+        rows.push(entry);
+      });
+
+      return { jobDetails: rows };
+    },
+  };
+});
